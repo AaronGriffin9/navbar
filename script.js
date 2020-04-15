c7b3c98df57169a918fc88787e088d8fd65f275d
AaronGriffin9 <44605619+AaronGriffin9@users.noreply.github.com>
2020-04-15 15:32:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById("navLink");
+let navBarToggle = document.getElementById("toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
